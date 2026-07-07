4ee5288523c0b4e75b064400a288ceb5b3c0238a
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-07-07 23:05:04 +0800

    emulationstation: bump es4all pin to b01141a (getShOutput unguard)

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 
 # es4all: 源码改由统一仓库 es4all 提供（原 ROCKNIX/emulationstation-next）。
 PKG_NAME="emulationstation"
-PKG_VERSION="bc14ae742c233d3740390d86f333e5986d7303e3"
+PKG_VERSION="b01141a5492505449486d0d6f7f6118a4132cdf8"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/w2xg2022/es4all"
